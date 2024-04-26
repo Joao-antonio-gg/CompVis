db7f58a0a23689f0821bad94aa624a95361d1c6f
Joao-antonio-gg <joaoagg@al.insper.edu.br>
2024-04-26 15:57:32 -0300

    Mais fotos

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -2,5 +2,5 @@ import sys
 
 from icrawler.builtin import GoogleImageCrawler,BingImageCrawler
 
-google_crawler = GoogleImageCrawler(storage={'root_dir': 'crawled5'})
-google_crawler.crawl(keyword='válvula esfera', max_num=1000)
+google_crawler = BingImageCrawler(storage={'root_dir': 'crawled6'})
+google_crawler.crawl(keyword='Chave Seccionadora Rotativa', max_num=1000)
--- scripts/rename.py
+++ scripts/rename.py
@@ -1,7 +1,7 @@
 import os
-path = '/Users/joaop/OneDrive/Área de Trabalho/Limbo EleModC/Compvis/Projeto/CompVis/scripts/crawled4'
+path = '/Users/joaop/OneDrive/Área de Trabalho/Limbo EleModC/Compvis/Projeto/CompVis/scripts/crawled6'
 files = os.listdir(path)
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, f'{index}.png'))
+    os.rename(os.path.join(path, file), os.path.join(path, f'{index+76}.png'))
 
     
